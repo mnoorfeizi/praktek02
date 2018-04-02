e56aec53619d9f729612e333fd8736dcdc810406
mnoorfeizi <32352478+mnoorfeizi@users.noreply.github.com>
2018-04-02 23:14:04 +0800

    mengisi nilai atribute

--- src/praktek02/bujursangkaraksi.java
+++ src/praktek02/bujursangkaraksi.java
@@ -5,6 +5,8 @@ package praktek02;
 public class bujursangkaraksi {
     public static void main(String[] args) {
          bujursangkar r1  = new bujursangkar();
+         r1.sisi1 = 7;
+         r1.sisi2 = 4;
          
     }
     
