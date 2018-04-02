764fc724be9d39afc5ec4ee38abaf7a5e26eb28c
mnoorfeizi <32352478+mnoorfeizi@users.noreply.github.com>
2018-04-02 23:11:27 +0800

    membuainstance object

--- src/praktek02/bujursangkaraksi.java
+++ src/praktek02/bujursangkaraksi.java
@@ -1,7 +1,11 @@
-e praktek02;
+
+package praktek02;
+
 
 public class bujursangkaraksi {
     public static void main(String[] args) {
-        
-          }
+         bujursangkar r1  = new bujursangkar();
+         
+    }
+    
 }
