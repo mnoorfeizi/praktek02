924865d310c5b2a33c14e7c2316f01a6367eb24b
mnoorfeizi <32352478+mnoorfeizi@users.noreply.github.com>
2018-04-02 22:57:58 +0800

    membuat atribut dan behapior

--- src/praktek02/bujursangkar.java
+++ src/praktek02/bujursangkar.java
@@ -3,4 +3,10 @@ package praktek02;
 public class bujursangkar {
     double sisi1;
     double sisi2;
+    void cetakinfo(){
+        System.out.println("=================");
+        System.out.println("sisi1:"+sisi1);
+        System.out.println("sisi2:"+sisi2);
+        System.out.println("=================");
+    }
 }
