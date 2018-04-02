15dbf733f22465aba502eccdfd66683026fe697c
mnoorfeizi <32352478+mnoorfeizi@users.noreply.github.com>
2018-04-02 23:35:10 +0800

    constructor sama dengan nama class

--- src/praktek02/bujursangkar.java
+++ src/praktek02/bujursangkar.java
@@ -3,6 +3,14 @@ package praktek02;
 public class bujursangkar {
     double sisi1;
     double sisi2;
+
+    public bujursangkar() {
+        sisi1=6;
+        sisi2=2;
+                
+    }
+    }
+    
     void cetakinfo(){
         System.out.println("=================");
         System.out.println("sisi1:"+sisi1);
@@ -15,6 +23,9 @@ public class bujursangkar {
         double luas;
         luas=sisi1*sisi2;
         return luas;
-
+    } 
+        
+    void cetakluas(){    
+        System.out.println("luasnya adalah: "+hitungluas());
 
 }
